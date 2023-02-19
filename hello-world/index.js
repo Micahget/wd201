db70ffbdd7416a628dde77e598a8d66eac6a661c
Mikiyas Getachew <mikiyasgetachew77@gmail.com>
2023-02-19 22:29:55 +0530

    Remove all the code and repalce it to implement fs module by importing it and executing different modules

--- hello-world/index.js
+++ hello-world/index.js
@@ -1,5 +1,36 @@
-const hello = () => {
-    console.log("Hello GitHub!");
+//importing fs module
+const fs = require("fs");
 
-}
-hello();
+//creating file using fs module using writeFile() function.
+fs.writeFile(
+  "sample.txt",
+  "Hello World. Welcome to Node.js File System module.",
+  (err) => {
+    if (err) throw err;
+    console.log("File created!");
+  }
+);
+
+//this is fs module method to readFile async
+fs.readFile("sample.txt", (err, data) => {
+  if (err) throw err;
+  console.log(data.toString());
+});
+
+//this is fs module method to update file using appeednFile method
+fs.appendFile("sample.txt", " This is my updated content", (err) => {
+  if (err) throw err;
+  console.log("File updated!");
+});
+
+//this is to rename file name
+fs.rename("sample.txt", "test.txt", (err) => {
+  if (err) throw err;
+  console.log("File name updated!");
+});
+
+//thisis to delete the file we created
+fs.unlink("test.txt", (err) => {
+  if (err) throw err;
+  console.log("File test.txt deleted successfully!");
+});
