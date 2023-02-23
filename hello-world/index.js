8ca2897a0e8ff83e92b90c3e3a3be77d5af1beb3
Mikiyas Getachew <mikiyasgetachew77@gmail.com>
2023-02-24 00:10:17 +0530

    Install Eslint everwhere it required and fix everything which should be fixed

--- hello-world/index.js
+++ hello-world/index.js
@@ -1,40 +1,37 @@
-//importing fs module
-const fs = require("fs");
+// importing fs module
+const fs = require('fs')
 
-//creating file using fs module using writeFile() function.
+// creating file using fs module using writeFile() function.
 fs.writeFile(
-  "sample.txt",
-  "Hello World. Welcome to Node.js File System module.",
+  'sample.txt',
+  'Hello World. Welcome to Node.js File System module.',
   (err) => {
-    if (err) throw err;
-    console.log("File created!");
+    if (err) throw err
+    console.log('File created!')
   }
-);
+)
 
-//this is fs module method to readFile async
-fs.readFile("sample.txt", (err, data) => {
-  if (err) throw err;
-  console.log(data.toString());
-});
+// this is fs module method to readFile async
+fs.readFile('sample.txt', (err, data) => {
+  if (err) throw err
+  console.log(data.toString())
+})
 
-//this is fs module method to update file using appeednFile method
-fs.appendFile("sample.txt", " This is my updated content", (err) => {
-  if (err) throw err;
-  console.log("File updated!");
-});
+// this is fs module method to update file using appeednFile method
+fs.appendFile('sample.txt', ' This is my updated content', (err) => {
+  if (err) throw err
+  console.log('File updated!')
+})
 
-//this is to rename file name
-fs.rename("sample.txt", "test.txt", (err) => {
-  if (err) throw err;
-  console.log("File name updated!");
-});
+// this is to rename file name
+fs.rename('sample.txt', 'test.txt', (err) => {
+  if (err) throw err
+  console.log('File name updated!')
+})
 
-//thisis to delete the file we created
-fs.unlink("test.txt", (err) => {
-  if (err) throw err;
-  console.log("File test.txt deleted successfully!");
-});
-//close file.
-fs.close;
-
-//add one line here
+// thisis to delete the file we created
+fs.unlink('test.txt', (err) => {
+  if (err) throw err
+  console.log('File test.txt deleted successfully!')
+})
+// close file.
