63de4dc4f76e4a75d36b7ebf1606f7f7c9dc6db1
Mikiyas Getachew <mikiyasgetachew77@gmail.com>
2023-02-24 16:57:26 +0530

    ADD: two html files; Project and home html files

--- http-server/index.js
+++ http-server/index.js
@@ -1,1 +1,19 @@
-import { ESLint } from "eslint";//here we install eslint globally so whenever we want to use eslint we can simply import it and use it.
+const http = require("http");
+const fs = require("fs");
+
+fs.readFile("home.html", (err, home) => {
+    console.log(home.toString());
+});
+
+fs.readFile("home.html", (err, home) => {
+    if (err) {
+        throw err;
+    }
+    http
+        .createServer((request, response) => {
+            response.writeHeader(200, { "Content-Type": "text/html" });
+            response.write(home);
+            response.end();
+        })
+        .listen(3000);
+});
