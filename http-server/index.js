336ef61d50334232b8ecaf741338ec3b6308c99f
Mikiyas Getachew <mikiyasgetachew77@gmail.com>
2023-02-24 17:14:34 +0530

    ADD: new registration.html form LINK: the new form to projects list

--- http-server/index.js
+++ http-server/index.js
@@ -1,19 +1,56 @@
+// const http = require("http");
+// const fs = require("fs");
+
+// fs.readFile("home.html", (err, home) => {
+//     console.log(home.toString());
+// });
+
+// fs.readFile("home.html", (err, home) => {
+//     if (err) {
+//         throw err;
+//     }
+//     http
+//         .createServer((request, response) => {
+//             response.writeHeader(200, { "Content-Type": "text/html" });
+//             response.write(home);
+//             response.end();
+//         })
+//         .listen(3000);
+// });
+
 const http = require("http");
 const fs = require("fs");
 
-fs.readFile("home.html", (err, home) => {
-    console.log(home.toString());
-});
+let homeContent = "";
+let projectContent = "";
 
 fs.readFile("home.html", (err, home) => {
     if (err) {
         throw err;
     }
-    http
-        .createServer((request, response) => {
-            response.writeHeader(200, { "Content-Type": "text/html" });
-            response.write(home);
-            response.end();
-        })
-        .listen(3000);
+    homeContent = home;
 });
+
+fs.readFile("project.html", (err, project) => {
+    if (err) {
+        throw err;
+    }
+    projectContent = project;
+});
+
+http
+    .createServer((request, response) => {
+        let url = request.url;
+        response.writeHeader(200, { "Content-Type": "text/html" });
+        switch (url) {
+            case "/project":
+                response.write(projectContent);
+                response.end();
+                break;
+            default:
+                response.write(homeContent);
+                response.end();
+                break;
+        }
+    })
+    .listen(3000);
