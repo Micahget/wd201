0017041809c27cf9acea449e02228b79da456522
Mikiyas Getachew <mikiyasgetachew77@gmail.com>
2023-02-24 17:52:24 +0530

    ADD: a command to accept input from cli using minimist CREATE: server to run the program by port which is accepted from cli

--- http-server/index.js
+++ http-server/index.js
@@ -1,23 +1,3 @@
-// const http = require("http");
-// const fs = require("fs");
-
-// fs.readFile("home.html", (err, home) => {
-//     console.log(home.toString());
-// });
-
-// fs.readFile("home.html", (err, home) => {
-//     if (err) {
-//         throw err;
-//     }
-//     http
-//         .createServer((request, response) => {
-//             response.writeHeader(200, { "Content-Type": "text/html" });
-//             response.write(home);
-//             response.end();
-//         })
-//         .listen(3000);
-// });
-
 const http = require("http");
 const fs = require("fs");
 
@@ -37,14 +17,26 @@ fs.readFile("project.html", (err, project) => {
     }
     projectContent = project;
 });
-
-http
-    .createServer((request, response) => {
+//here we are reading the registration.html file and storing it in registrationContent variable.
+fs.readFile("registration.html", (err, home) => {
+    if (err) {
+        throw err;
+    }
+    registrationContent = home;
+});
+/*
+//here we are creating the server and listening to the port 3000
+http.createServer((request, response) => {
         let url = request.url;
         response.writeHeader(200, { "Content-Type": "text/html" });
         switch (url) {
+            //here we are checking the url and sending the appropriate content
             case "/project":
                 response.write(projectContent);
+                response.end();
+                break;
+            case "/registration":
+                response.write(registrationContent);
                 response.end();
                 break;
             default:
@@ -54,3 +46,36 @@ http
         }
     })
     .listen(3000);
+*/
+
+//using minimist to accept cli input and using default port 3000 if no port is provided in cli
+const minimist = require('minimist')
+const args = minimist(process.argv.slice(2), {
+   default: {
+     port: 3000
+      }})
+const port = args.port
+
+//here we are creating the server and listening to the cli input port
+http.createServer((request, response) => {
+    let url = request.url;
+    response.writeHeader(200, { "Content-Type": "text/html" });
+    switch (url) {
+        case "/project":
+            response.write(projectContent);
+            response.end();
+            break;
+        case "/registration":
+            response.write(registrationContent);
+            response.end();
+            break;
+        default:
+            response.write(homeContent);
+            response.end();
+            break;
+    }
+}).listen(port);
+
+
+
+
