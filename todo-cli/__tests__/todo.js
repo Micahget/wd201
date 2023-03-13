185dd2c3569b72ebd6e928e8853787b217b4f983
Mikiyas Getachew <mikiyasgetachew77@gmail.com>
2023-03-13 22:45:54 +0530

    ADD: to randomw date generators from past and future change the unit tests by addint more stuff to be checked for all three methods : add some tests

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -5,7 +5,19 @@
 
 const todoList = require("../todo");
 
-const { all, markAsComplete, add } = todoList();
+const { all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
+const getRandomDateInFuture = () => {
+  const today = new Date();
+  const tomorrow = new Date(today);
+  tomorrow.setDate(tomorrow.getDate() + 100 * Math.random());
+  return tomorrow.toISOString().slice(0, 10);
+};
+const getRandomDateInPast = () => {
+  const today = new Date();
+  const yesterday = new Date(today);
+  yesterday.setDate(yesterday.getDate() - 100 * Math.random());
+  return yesterday.toISOString().slice(0, 10);
+};
 
 describe("todoList test suite", () => {
   beforeAll(() => {
@@ -30,25 +42,39 @@ describe("todoList test suite", () => {
     markAsComplete(0);
     expect(all[0].completed).toBe(true);
   });
-  // overdue
-  test("...checks retrieval of overdue items", () => {
-    const overdue = all.filter((item) => {
-      return item.dueDate < new Date().toISOString().slice(0, 10);
+  // Overdue
+  test("retrieval of overdue items", () => {
+    const overdueItems = overdue(); // directly used the method instead of writing the logic for filter
+    expect(overdueItems.length).toBe(0);
+    add({
+      title: "Finish the assignment",
+      dueDate: getRandomDateInPast(),
+      completed: false,
     });
-    expect(overdue.length).toBe(0);
+    expect(overdue().length).toBe(overdueItems.length + 1);
   });
   // due today
-  test("...checks retrieval of due today items", () => {
-    const dueToday = all.filter((item) => {
-      return item.dueDate === new Date().toISOString().slice(0, 10);
+  test("retrieval of due today's items", () => {
+    const duetodayItems = dueToday(); // directly used the method instead of writing the logic for filter
+    expect(duetodayItems.length).toBe(2);
+    add({
+      title: "Finish the assignment",
+      // get todays Date
+      dueDate: new Date().toISOString().slice(0, 10),
+      completed: false,
     });
-    expect(dueToday.length).toBe(2);
+    expect(dueToday().length).toBe(duetodayItems.length + 1);
   });
   // duelater
-  test("...checks retrieval of due later items", () => {
-    const dueLater = all.filter((item) => {
-      return item.dueDate > new Date().toISOString().slice(0, 10);
+  test("retrieval of duelater items", () => {
+    const duelaterItems = dueLater(); // directly used the method instead of writing the logic for filter
+    expect(duelaterItems.length).toBe(0);
+    add({
+      title: "Finish the assignment",
+      // get random day in future
+      dueDate: getRandomDateInFuture(),
+      completed: false,
     });
-    expect(dueLater.length).toBe(0);
+    expect(dueLater().length).toBe(duelaterItems.length + 1);
   });
 });
