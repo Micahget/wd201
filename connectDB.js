f95c0f7148e0b99010aba00fc0488a2006465a16
Mikiyas Getachew <mikiyasgetachew77@gmail.com>
2023-03-16 23:23:47 +0530

    LEVEL 5: Database with Postgrasql. Install: postgrasql Sequelize module ADD: connectDB.js file. and connect with database sucessfully

---
+++ connectDB.js
@@ -0,0 +1,20 @@
+// connectDB.js
+
+const Sequelize = require("sequelize");
+
+const database = "todo_db";
+const username = "postgres";
+const password = "mypostgres";
+const sequelize = new Sequelize(database, username, password, {
+  host: "localhost",
+  dialect: "postgres",
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((error) => {
+    console.error("Unable to connect to the database:", error);
+  });
