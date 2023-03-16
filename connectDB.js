bd54a29f9780b7ae77bc9ba54e07f9921d1336a1
Mikiyas Getachew <mikiyasgetachew77@gmail.com>
2023-03-16 23:25:47 +0530

    MODIFY: modify the connectDB.js file to export to functions

--- connectDB.js
+++ connectDB.js
@@ -10,11 +10,11 @@ const sequelize = new Sequelize(database, username, password, {
   dialect: "postgres",
 });
 
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log("Connection has been established successfully.");
-  })
-  .catch((error) => {
-    console.error("Unable to connect to the database:", error);
-  });
+const connect = async () => {
+    return sequelize.authenticate();
+  }
+  
+  module.exports = {
+    connect,
+    sequelize
+  }
