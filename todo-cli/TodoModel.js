048b51757e17eb313e2c9556c306bff87427ea6c
Mikiyas Getachew <mikiyasgetachew77@gmail.com>
2023-03-21 15:46:55 +0530

    Rename Folder listTodos.js

--- todo-cli/TodoModel.js
+++ todo-cli/TodoModel.js
@@ -1,14 +1,16 @@
-const { DataTypes, Model } = require('sequelize')
-const { sequelize } = require('./connectDB.js')
+/* eslint-disable */
+
+const { DataTypes, Model } = require("sequelize");
+const { sequelize } = require("./connectDB.js");
 
 class Todo extends Model {
   // create a static method to create a todo item. which will be used in the index.js file. and its useful to perform validations on the data before saving it to the database.
-  static async addTodo (params) {
-    return await Todo.create(params)
+  static async addTodo(params) {
+    return await Todo.create(params);
   }
 
-  displayableString () {
-    return (`${this.id} ${this.title} ${this.dueDate} ${this.completed}`)
+  displayableString() {
+    return `${this.id} ${this.title} ${this.dueDate} ${this.completed}`;
   }
 }
 
@@ -17,19 +19,19 @@ Todo.init(
     // Model attributes are defined here
     title: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
     },
     dueDate: {
-      type: DataTypes.DATEONLY
+      type: DataTypes.DATEONLY,
     },
     completed: {
-      type: DataTypes.BOOLEAN
-    }
+      type: DataTypes.BOOLEAN,
+    },
   },
   {
-    sequelize
+    sequelize,
   }
-)// here the name of the table is todo
+); // here the name of the table is todo
 
-Todo.sync()
-module.exports = Todo
+Todo.sync();
+module.exports = Todo;
--- todo-cli/index.js
+++ todo-cli/index.js
@@ -1,94 +1,91 @@
 /* eslint-disable */
-const { connect } = require("./connectDB.js");// this is the connection to the database.
+const { connect } = require("./connectDB.js"); // this is the connection to the database.
 const { count } = require("./TodoModel.js");
 const Todo = require("./TodoModel.js"); // this is the model for the table.
 
 const createTodo = async (title, dueDate, completed) => {
-    try {
-        await connect();
-        const todo = await Todo.addTodo({
-            title: title,
-            dueDate: dueDate,
-            completed: completed,
-        });
+  try {
+    await connect();
+    const todo = await Todo.addTodo({
+      title: title,
+      dueDate: dueDate,
+      completed: completed,
+    });
 
-        console.log(`Created todo with ID : ${todo.id}`);
-    } catch (error) {
-
-        console.error(error);
-
-
-    }
-}
+    console.log(`Created todo with ID : ${todo.id}`);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 const countItems = async () => {
-    try {
-        const totalCount = await Todo.count();
-        console.log(`Found ${totalCount} items in the database`);
-    } catch (error) {
-        console.error(error);
-
-    }
-}
+  try {
+    const totalCount = await Todo.count();
+    console.log(`Found ${totalCount} items in the database`);
+  } catch (error) {
+    console.error(error);
+  }
+};
 // to fetch the data from the table
 const getAllTodos = async () => {
-    try {
-        const todos = await Todo.findAll()
-        const pring = todos.map((todo) => todo.displayableString()).join("\n");
-        console.log(pring);
-    } catch (error) {
-        console.error(error);
-    }
-}
+  try {
+    const todos = await Todo.findAll();
+    const pring = todos.map((todo) => todo.displayableString()).join("\n");
+    console.log(pring);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 // to get single todo
 
 const getSingleTodo = async () => {
-    try {
-        const todo = await Todo.findOne({
-            where: {
-                completed: false
-            },
-            order: [
-                ['id', 'DESC']
-            ]
-        })
-        console.log(todo.displayableString());
-    } catch (error) {
-        console.error(error);
-    }
-}
+  try {
+    const todo = await Todo.findOne({
+      where: {
+        completed: false,
+      },
+      order: [["id", "DESC"]],
+    });
+    console.log(todo.displayableString());
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 // to update the data in the table
 const updatItem = async (title, dueDate, completed) => {
-    try {
-        await Todo.update({
-            title: title,
-            dueDate: dueDate,
-            completed: completed
-        }, {
-            where: {
-                id: 4
-            }
-        })
-    } catch (error) {
-        console.error(error);
-    }
-}
+  try {
+    await Todo.update(
+      {
+        title: title,
+        dueDate: dueDate,
+        completed: completed,
+      },
+      {
+        where: {
+          id: 4,
+        },
+      }
+    );
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 // deleteing the data from the table
 const deleteItem = async (id) => {
-    try {
-        const deletedRowCount = await Todo.destroy({
-            where: {
-                id: id
-            }
-        })
-        console.log(`Number of rows deleted: ${deletedRowCount}`)
-    } catch (error) {
-        console.error(error);
-    }
-}
+  try {
+    const deletedRowCount = await Todo.destroy({
+      where: {
+        id: id,
+      },
+    });
+    console.log(`Number of rows deleted: ${deletedRowCount}`);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 // this is the main function that will be called when the script is run. It is called an IIFE (Immediately Invoked Function Expression).
 // (async () => {
@@ -103,7 +100,6 @@ const deleteItem = async (id) => {
 //     // await getSingleTodo();
 // })();
 
-
 /*
     this is to show the asyncronus nature of javascript
 
@@ -111,22 +107,20 @@ const deleteItem = async (id) => {
 // getAllTodos();
 // countItems(); // this will be printed first even if it is not called first because of the the asyncronus nature of javascript. To avoid this we can use async await or promises.
 
-
 /*
 
     this is to show the async await nature of javascript
 
 */
 const run = async () => {
-    await getAllTodos();
-    await countItems();
-}
+  await getAllTodos();
+  await countItems();
+};
 
-run();// now the output will be printed in the correct order. because the run function is async and it will wait for the getAllTodos and countItems to finish before printing the output.
+run(); // now the output will be printed in the correct order. because the run function is async and it will wait for the getAllTodos and countItems to finish before printing the output.
 
 // invoking the fun anoniomus function
 (async () => {
-    await getAllTodos();
-    await countItems();
-}
-)();
+  await getAllTodos();
+  await countItems();
+})();
