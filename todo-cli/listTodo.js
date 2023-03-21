8139bf66c1baaa38f5d93e5a7260b73a204d18ea
Mikiyas Getachew <mikiyasgetachew77@gmail.com>
2023-03-21 15:34:33 +0530

    First commit after revert the previous changes. all the tests has passed. git hook is also fixed. Now It is REady for submittion

---
+++ todo-cli/listTodo.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+
+//  listTodos.js
+const db = require("./models/index");
+
+const listTodo = async () => {
+  try {
+    await db.Todo.showList();
+  } catch (error) {
+    console.error(error);
+  }
+};
+(async () => {
+  await listTodo();
+})();
