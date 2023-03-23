6002b8f9ec0394f5b7a0550431ecc62eb84a81b5
Mikiyas Getachew <mikiyasgetachew77@gmail.com>
2023-03-23 16:54:49 +0530

    install postgres database and install Sequlizer module and sequlizer-cli as well. and configure all

--- todo-app/index.js
+++ todo-app/index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 
 
-app.get("/", (request , response) => {
+app.get("/", (request, response) => {
     response.send("Hello World!");
-}
-);
+}); // here we are listening get request on the root route. if we want to listen post request we can use app.post() method
 
 // the below code is to make the server listen to port 3000
+// app.listen(3000);
 app.listen(5555, () => {
     console.log("Server is running on port 3000");
 }
