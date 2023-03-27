6a90e60d8dae6cb6f53cc4174813c04127fb3abb
Mikiyas Getachew <mikiyasgetachew77@gmail.com>
2023-03-27 16:23:34 +0530

    test pass

--- todo-app/migrations/20230324093813-create-todo.js
+++ todo-app/migrations/20230324093813-create-todo.js
@@ -1,34 +1,36 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Todos', {
-      id: {// this comes from the model file 
+    await queryInterface.createTable("Todos", {
+      id: {
+        // this comes from the model file
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       title: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       dueDate: {
-        type: Sequelize.DATEONLY
+        type: Sequelize.DATEONLY,
       },
       completed: {
-        type: Sequelize.BOOLEAN
+        type: Sequelize.BOOLEAN,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
+  /* eslint-disable no-unused-vars */
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Todos');
-  }
+    await queryInterface.dropTable("Todos");
+  },
 };
