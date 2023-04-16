162ee0fadb46f4bd829e3907a3e73cbc7092ca93
Mikiyas Getachew <mikiyasgetachew77@gmail.com>
2023-04-16 14:53:49 +0530

    ADD: Authentication using middleware moduels: passport.js, express-session,connect-ensure-login, passport-local DETAIL: passport login- for authentication purpose, express-session- to save users credantioal after logged in connect-ensure-login- to make private routes which will be accessable through login only passport-local - our way of authentication/ method of authentication, we can use google, facebook or other methods as well

--- todo-app/app.js
+++ todo-app/app.js
@@ -5,22 +5,85 @@ var csrf = require("tiny-csrf");
 // define cookie parser
 var cookieParser = require("cookie-parser");
 const app = express();
-const { Todo } = require("./models");
+const { Todo, User } = require("./models");
 const bodyParser = require("body-parser");
 const path = require("path"); // here we are using path module to get the path of the public folder
+
+// import authentication middlewares
+const passport = require("passport");
+const session = require("express-session");
+const connectEnsureLogin = require("connect-ensure-login");
+const LocalStrategy = require("passport-local").Strategy;
+
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
-/* this is to post data from the form. It is a middleware that parses incoming requests with urlencoded payloads and is based on body-parser. It is used to parse the data that the user submits in the form. And it is used to parse the data that is sent in the request body.*/
+/* this is to post data from the form. It 
+is a middleware that parses incoming requests with urlencoded payloads and is based on body-parser. It is used to parse the data that the user submits in the form. And it is used to parse the data that is sent in the request body.*/
+
 app.use(cookieParser("shh! some secret string"));
 app.use(csrf("this_should_be_32_character_long", ["POST", "PUT", "DELETE"]));
+
+// configure passport.js to use the session
+app.use(
+  session({
+    secret: "my_super_secret_key-2345235234534534534",
+    cookie: { maxAge: 24 * 60 * 60 * 1000 }, // 24 hours
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// configure passport.js to use the local strategy
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+    },
+    (username, password, done) => {
+      User.findOne({ where: { email: username, password: password } }).then((user) => { // here user is the object returned from the database
+        return done(null, user); // here done is a function that is called when the authentication is successful
+      }).catch((error) => {
+        return done(error);
+      });
+    }
+  )
+);
+
+// tell passport how to serialize the user
+/* here serialize means 
+ convert an object into a string. And deserialize 
+means to convert a string into an object.
+ So, here we are telling passport how to convert the user object into a string. And we are using the user id to do that. */
+/* the reson why we are serializing 
+is b/c we want to store the user id in the session.
+ And we want to store the user id in the session b/c we want to know which user is logged in. */
+passport.serializeUser((user, done) => {
+  console.log("serializing user in session ", user.id);
+  done(null, user.id);
+});
+
+// tell passport how to deserialize the user
+passport.deserializeUser((id, done) => {
+  User.findByPk(id).then((user) => {
+    console.log("deserializing user in session ", user.id);
+    done(null, user);
+  }).catch((error) => {
+    done(error, null);
+  });
+});
 
 // set the view engine to ejs
 app.set("view engine", "ejs");
 
-/*// we add this get route to render the index.ejs file in the views folder and pass the data to it using the allTodos variable which is an array of all the todos in the database*/
+/*// we add this get route to
+ render t
+ he index.ejs file in the views folder and pass the data to it using the allTodos variable which is an array of all the todos in the database*/
 
 /*
-// this one is my implementation. TO USE THIS, YOU SHOULD MAKE SOME CHANGES IN THE TODOS.EJS FILE AND TODO.JS FILE IN TESTS FOLDER.
+// this one is my implementation. TO USE THIS, YOU SHOULD MAKE SOME CHANGES IN THE TODOS
+.EJS FILE AND TODO.JS FILE IN TESTS FOLDER.
 app.get("/", async (request, response) => {
   const allTodos = await Todo.getTodos();
   if (request.accepts("html")) {
@@ -35,14 +98,25 @@ app.get("/", async (request, response) => {
 }); 
 */
 
+// this is root route and it is public
 app.get("/", async (request, response) => {
+  response.render("index", {
+    title: "Todo Application",
+    csrfToken: request.csrfToken(),
+  });
+
+});
+
+/*// here this 
+page is private page and it is protected by the authentication middleware that is what connectEnsureLogin.ensureLoggedIn() does */
+app.get("/todos", connectEnsureLogin.ensureLoggedIn(), async function (request, response) {
   const overdue = await Todo.overdue();
   const dueToday = await Todo.dueToday();
   const dueLater = await Todo.dueLater();
   const completedItem = await Todo.completedItem();
 
   if (request.accepts("html")) {
-    response.render("index", {
+    response.render("todos", {
       title: "Todo Application",
       overdue,
       dueToday,
@@ -58,7 +132,8 @@ app.get("/", async (request, response) => {
       completedItem,
     });
   }
-}); // the above code is not working b/c the overdue, dueToday and dueLater are not defined in the Todo model. Now they have been defined.
+});
+
 
 // to render files from the public folder
 app.use(express.static(path.join(__dirname, "public"))); // here we are using path module to get the path of the public folder
@@ -78,6 +153,7 @@ app.get("/todos", async function (_request, response) {
   // response.send(todos)
 });
 
+
 app.get("/todos/:id", async function (request, response) {
   try {
     const todo = await Todo.findByPk(request.params.id);
@@ -88,6 +164,38 @@ app.get("/todos/:id", async function (request, response) {
   }
 });
 
+// render the login page
+app.get("/signup", function (request, response) {
+  response.render("signup", { title: "Signup", csrfToken: request.csrfToken() });
+});
+
+
+// creating users route to render the signup.ejs file
+app.post("/users", async function (request, response) {
+  console.log("first name", request.body.firstName);
+  try {
+    const user = await User.create({
+      firstName: request.body.firstName,
+      lastName: request.body.lastName,
+      email: request.body.email,
+      password: request.body.password,
+
+    });
+    // here we should initialize the session
+    request.login(user, (error) => {
+      if (error) {
+        console.log(error);
+        return response.status(422).json(error);
+      }
+      return response.redirect("/todos");
+    })
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
+
+
 app.post("/todos", async function (request, response) {
   try {
     const todo = await Todo.addTodo(request.body);
@@ -97,6 +205,7 @@ app.post("/todos", async function (request, response) {
     return response.status(422).json(error);
   }
 });
+
 
 app.put("/todos/:id", async function (request, response) {
   const todo = await Todo.findByPk(request.params.id);
